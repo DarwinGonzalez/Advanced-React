b58947e18cd8d2a264afd7e80f5365a574d4976a
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-12-02 11:23:46 +0100

    Setting up Apollo Client

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,13 @@
 const Mutations = {
-    createDog(parent, args, ctx, info) {
-        global.dogs = global.dogs || [];
-        // create a dog!
-        const newDog = { name: args.name };
-        global.dogs.push(newDog);
-        return newDog;
+
+    async createItem(parent, args, ctx, info) {
+        //TODO: Check if they are log in
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+        return item;
     }
 };
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,32 @@
 import App, { Container } from 'next/app';
 import Page from '../components/Page';
-class MyApp extends App {
-    render() {
-        const { Component } = this.props;
+import { ApolloProvider } from 'react-apollo';
+import withData from '../lib/withData';
 
-        return(
-            <Container>
-                <Page>
-                <Component></Component>
-                </Page>
-            </Container>
-        )
+class MyApp extends App {
+    static async getInitialProps({ Component, ctx}) {
+        let pageProps = {};
+        if(Component.getInitialProps) {
+            pageProps = await Component.getInitialProps(ctx);
+        }
+        // This exposes the query to the user
+        pageProps.query = ctx.query;
+        return { pageProps };
     }
+
+	render() {
+		const { Component, apollo, pageProps } = this.props;
+
+		return (
+			<Container>
+				<ApolloProvider client={apollo}>
+					<Page>
+						<Component {...pageProps}/>
+					</Page>
+				</ApolloProvider>
+			</Container>
+		);
+	}
 }
 
-export default MyApp;
+export default withData(MyApp);
