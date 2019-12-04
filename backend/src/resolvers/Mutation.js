dd85d088c5dd0b56bf635d8536418c9e67ced020
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-12-04 13:13:22 +0100

    Finally implement log out button

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -81,6 +81,10 @@ const Mutations = {
         });
         // 5. Return the user
         return user;
+    },
+    signout(parent, args, ctx, info) {
+        ctx.response.clearCookie('token');
+        return { message: 'Goddbye!' };
     }
 };
 
