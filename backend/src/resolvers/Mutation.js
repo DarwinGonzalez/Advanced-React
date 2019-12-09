dcc8328689b7cf01021a26ffa1303fc9691e6373
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-12-09 17:22:20 +0100

    Finally updating permissions succesfully

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -2,163 +2,204 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { randomBytes } = require('crypto');
 const { promisify } = require('util');
-const {transport, makeANiceEmail } = require('../mail');
+const { transport, makeANiceEmail } = require('../mail');
+const { hasPermission } = require('../utils');
 
 const Mutations = {
+	async createItem(parent, args, ctx, info) {
+		// Check if they are log in
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in to do that!');
+		}
+		const item = await ctx.db.mutation.createItem(
+			{
+				data: {
+					// This is how we create a relationship between the Item and the User
+					user: {
+						connect: {
+							id: ctx.request.userId
+						}
+					},
+					...args
+				}
+			},
+			info
+		);
+		return item;
+	},
 
-    async createItem(parent, args, ctx, info) {
-        // Check if they are log in
-        if(!ctx.request.userId) {
-            throw new Error('You must be logged in to do that!');
-        }
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                // This is how we create a relationship between the Item and the User
-                user: {
-                    connect: {
-                        id: ctx.request.userId,
-                    }
-                },
-                ...args,
-            }
-        }, info);
-        return item;
-    },
-
-    async updateItem(parent, args, ctx, info) {
-        // Fisrt take a copy of the update
-        const updates = { ...args }
-        // Remove the ID from the updates
-        delete updates.id;
-        // Run the update metthod
-        const updatedItem = await ctx.db.mutation.updateItem({
-            data: updates,
-            where: {
-                id: args.id
-            }
-        }, info);
-        return updatedItem;
-    },
-    async deleteItem(parent, args, ctx, info) {
-        const where = { id: args.id };
-        // 1. Find the item
-        const item = await ctx.db.query.item({ where }, `{id, title}`);
-        // 2. Check if they own that item, or have the permissions
-        // TODO
-        // 3. Delete it!
-        return ctx.db.mutation.deleteItem({ where }, info);
-    },
-    async signup(parent, args, ctx, info) {
-        // Lower case their email
-        args.email = args.email.toLowerCase();
-        // hash their password
-        const password = await bcrypt.hash(args.password, 10);
-        // create user in the database
-        const user = await ctx.db.mutation.createUser({
-            data: {
-                ...args,
-                password,
-                permissions: { set: ['USER']},
-            }
-        }, info);
-        // Create the JWT token for the user
-        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
-        // We set the JWT as a cookie on the response
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-        });
-        //Finally we return the user to the browser
-        return user;
-    },
-    async signin(parent, { email, password}, ctx, info) {
-        // 1. Ckeck if there is a user with that email
-        const user =  await ctx.db.query.user({
-            where: { email }
-        });
-        if(!user) {
-            throw new Error(`No such user found for email ${email}`);
-        }
-        // 2. Check if their password is correct
-        const valid = await bcrypt.compare(password, user.password);
-        if(!valid) {
-            throw new Error('Invalid password!');
-        }
-        // 3. Generate the JWT Token
-        const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
-        // 4. Set that cookie with the token
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-        });
-        // 5. Return the user
-        return user;
-    },
-    signout(parent, args, ctx, info) {
-        ctx.response.clearCookie('token');
-        return { message: 'Goddbye!' };
-    },
-    async requestReset(parent, args, ctx, info) {
-        // 1. Check if this is a real user
-        const user = await ctx.db.query.user({ where: { email: args.email }});
-        if(!user) {
-            throw new Error(`No such user found for email ${args.email}`);
-        }
-        // 2. Set a reset token and expiry on that user
-        const randomBytesPromiseified = promisify(randomBytes)
-        const resetToken = (await randomBytesPromiseified(20)).toString('hex');
-        const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
-        const res = await ctx.db.mutation.updateUser({
-            where: { email: args.email },
-            data: { resetToken, resetTokenExpiry }
-        });
-        // 3. Email them reset token
-        const mailRes =  await transport.sendMail({
-            from: 'darwinsender@gmail.com',
-            to: user.email,
-            subject: 'Your password token',
-            html: `Your password reset Token is here! \n\n <a href="${process.env.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to Reset</a>`
-        })
-        // 4. Return the message
-        return { message: 'Thanks!'};
-    },
-    async resetPassword(parent, args, ctx, info) {
-        // Check if the paswords macth
-        if(args.password !== args.confirmPassword) {
-            throw new Error('Passwords dont match!');
-        }
-        // 2. Check if is a legit reset token
-        // 3.  Check if its expired
-        const [user] = await ctx.db.query.users({
-            where: {
-                resetToken: args.resetToken,
-                resetTokenExpiry_gte: Date.now() - 3600000
-            }
-        });
-        if(!user) {
-            throw new Error('This token is either invalid or expired');
-        }
-        // 4. Hash their new password
-        const password = await bcrypt.hash(args.password, 10);
-        // 5. Save the new password to the user and remove old resetToken fields
-        const updatedUser =  await ctx.db.mutation.updateUser({
-            where: { email: user.email },
-            data: {
-                password,
-                resetToken: null,
-                resetTokenExpiry: null,
-            }
-        })
-        // 6. Generate JWT
-        const token = jwt.sign({userId: updatedUser.id}, process.env.APP_SECRET);
-        // 7. sET THE JWT cookie
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-        });
-        // 8. Return the new user
-        return updatedUser;
-    }
+	async updateItem(parent, args, ctx, info) {
+		// Fisrt take a copy of the update
+		const updates = { ...args };
+		// Remove the ID from the updates
+		delete updates.id;
+		// Run the update metthod
+		const updatedItem = await ctx.db.mutation.updateItem(
+			{
+				data: updates,
+				where: {
+					id: args.id
+				}
+			},
+			info
+		);
+		return updatedItem;
+	},
+	async deleteItem(parent, args, ctx, info) {
+		const where = { id: args.id };
+		// 1. Find the item
+		const item = await ctx.db.query.item({ where }, `{id, title}`);
+		// 2. Check if they own that item, or have the permissions
+		// TODO
+		// 3. Delete it!
+		return ctx.db.mutation.deleteItem({ where }, info);
+	},
+	async signup(parent, args, ctx, info) {
+		// Lower case their email
+		args.email = args.email.toLowerCase();
+		// hash their password
+		const password = await bcrypt.hash(args.password, 10);
+		// create user in the database
+		const user = await ctx.db.mutation.createUser(
+			{
+				data: {
+					...args,
+					password,
+					permissions: { set: [ 'USER' ] }
+				}
+			},
+			info
+		);
+		// Create the JWT token for the user
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// We set the JWT as a cookie on the response
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		//Finally we return the user to the browser
+		return user;
+	},
+	async signin(parent, { email, password }, ctx, info) {
+		// 1. Ckeck if there is a user with that email
+		const user = await ctx.db.query.user({
+			where: { email }
+		});
+		if (!user) {
+			throw new Error(`No such user found for email ${email}`);
+		}
+		// 2. Check if their password is correct
+		const valid = await bcrypt.compare(password, user.password);
+		if (!valid) {
+			throw new Error('Invalid password!');
+		}
+		// 3. Generate the JWT Token
+		const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+		// 4. Set that cookie with the token
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		// 5. Return the user
+		return user;
+	},
+	signout(parent, args, ctx, info) {
+		ctx.response.clearCookie('token');
+		return { message: 'Goddbye!' };
+	},
+	async requestReset(parent, args, ctx, info) {
+		// 1. Check if this is a real user
+		const user = await ctx.db.query.user({ where: { email: args.email } });
+		if (!user) {
+			throw new Error(`No such user found for email ${args.email}`);
+		}
+		// 2. Set a reset token and expiry on that user
+		const randomBytesPromiseified = promisify(randomBytes);
+		const resetToken = (await randomBytesPromiseified(20)).toString('hex');
+		const resetTokenExpiry = Date.now() + 3600000; // 1 hour from now
+		const res = await ctx.db.mutation.updateUser({
+			where: { email: args.email },
+			data: { resetToken, resetTokenExpiry }
+		});
+		// 3. Email them reset token
+		const mailRes = await transport.sendMail({
+			from: 'darwinsender@gmail.com',
+			to: user.email,
+			subject: 'Your password token',
+			html: `Your password reset Token is here! \n\n <a href="${process.env
+				.FRONTEND_URL}/reset?resetToken=${resetToken}">Click here to Reset</a>`
+		});
+		// 4. Return the message
+		return { message: 'Thanks!' };
+	},
+	async resetPassword(parent, args, ctx, info) {
+		// Check if the paswords macth
+		if (args.password !== args.confirmPassword) {
+			throw new Error('Passwords dont match!');
+		}
+		// 2. Check if is a legit reset token
+		// 3.  Check if its expired
+		const [ user ] = await ctx.db.query.users({
+			where: {
+				resetToken: args.resetToken,
+				resetTokenExpiry_gte: Date.now() - 3600000
+			}
+		});
+		if (!user) {
+			throw new Error('This token is either invalid or expired');
+		}
+		// 4. Hash their new password
+		const password = await bcrypt.hash(args.password, 10);
+		// 5. Save the new password to the user and remove old resetToken fields
+		const updatedUser = await ctx.db.mutation.updateUser({
+			where: { email: user.email },
+			data: {
+				password,
+				resetToken: null,
+				resetTokenExpiry: null
+			}
+		});
+		// 6. Generate JWT
+		const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET);
+		// 7. sET THE JWT cookie
+		ctx.response.cookie('token', token, {
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24 * 365 // 1 year cookie
+		});
+		// 8. Return the new user
+		return updatedUser;
+	},
+	async updatePermissions(parent, args, ctx, info) {
+		// 1. Check if they are logged in
+		if (!ctx.request.userId) {
+			throw new Error('You must be logged in!');
+		}
+		// 2. Query the current user
+		const currentUser = await ctx.db.query.user(
+			{
+				where: {
+					id: ctx.request.userId
+				}
+			},
+			info
+		);
+		// 3. Check if they have permissions to do this
+		hasPermission(currentUser, [ 'ADMIN', 'PERMISSIONUPDATE' ]);
+		// 4. Update the permissions
+		return await ctx.db.mutation.updateUser(
+			{
+				data: {
+					permissions: {
+						set: args.permissions
+					}
+				},
+				where: {
+					id: args.userId
+				}
+			},
+			info
+		);
+	}
 };
 
 module.exports = Mutations;
--- frontend/components/Permissions.js
+++ frontend/components/Permissions.js
@@ -1,27 +1,32 @@
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import Error from './ErrorMessage';
 import gql from 'graphql-tag';
-import Table from './styles/Table'
+import PropTypes from 'prop-types';
+import Table from './styles/Table';
 import SickButton from './styles/SickButton';
 
-const possiblePermissions = [
-    'ADMIN',
-    'USER',
-    'ITEMCREATE',
-    'ITEMUPDATE',
-    'ITEMDELETE',
-    'PERMISSIONUPDATE'
-];
+const possiblePermissions = [ 'ADMIN', 'USER', 'ITEMCREATE', 'ITEMUPDATE', 'ITEMDELETE', 'PERMISSIONUPDATE' ];
+
+const UPDATE_PERMISIIONS_MUTATION = gql`
+	mutation updatePermissions($permissions: [Permission], $userId: ID!) {
+		updatePermissions(permissions: $permissions, userId: $userId) {
+			id
+			permissions
+			name
+			email
+		}
+	}
+`;
 
 const ALL_USERS_QUERY = gql`
-    query {
-        users {
-            id
-            name
-            email
-            permissions
-        }
-    }
+	query {
+		users {
+			id
+			name
+			email
+			permissions
+		}
+	}
 `;
 
 const Permissions = (props) => (
@@ -29,46 +34,87 @@ const Permissions = (props) => (
 		{({ data, loading, error }) => (
 			<div>
 				<Error error={error} />
-                <h2>Manage Permissions</h2>
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            {possiblePermissions.map(permission => <th>{permission}</th>)}
-                            <th>
-                                👇
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.users.map(user => <User user={user}></User>)}
-                    </tbody>
-                </Table>
+				<h2>Manage Permissions</h2>
+				<Table>
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email</th>
+							{possiblePermissions.map((permission) => <th key={permission}>{permission}</th>)}
+							<th>👇</th>
+						</tr>
+					</thead>
+					<tbody>{data.users.map((user) => <UserPermissions user={user} key={user.id} />)}</tbody>
+				</Table>
 			</div>
 		)}
 	</Query>
 );
 
-class User extends React.Component {
-    render() {
-        const user = this.props.user;
-        return (
-            <tr>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-                {possiblePermissions.map(permission => (
-                    <td>
-                        <label htmlFor={`${user.id}-permission-${permission}`}></label>
-                        <input type="checkbox"></input>
-                    </td>
-                ))}
-                <td>
-                    <SickButton>Update</SickButton>
-                </td>
-            </tr>
-        )
-    }
+class UserPermissions extends React.Component {
+	static propTypes = {
+		user: PropTypes.shape({
+			name: PropTypes.string,
+			email: PropTypes.string,
+			id: PropTypes.string,
+			Permissions: PropTypes.array
+		}).isRequired
+	};
+	state = {
+		permissions: this.props.user.permissions
+	};
+	handlePermissionChange = (e) => {
+		const checkbox = e.target;
+		// take a copy of the current permissions
+		let updatedPermissions = [ ...this.state.permissions ];
+		// Figure out if we new to remove or add this permissions
+		if (checkbox.checked) {
+			// add it in!
+			updatedPermissions.push(checkbox.value);
+		} else {
+			updatedPermissions = updatedPermissions.filter((permission) => permission !== checkbox.value);
+		}
+		this.setState({ permissions: updatedPermissions });
+	};
+	render() {
+		const user = this.props.user;
+		return (
+			<Mutation
+				mutation={UPDATE_PERMISIIONS_MUTATION}
+				variables={{
+					permissions: this.state.permissions,
+					userId: this.props.user.id
+				}}
+			>
+				{(updatePermissions, { loading, error }) => (
+                    <>
+                    {error && <tr><td colSpan="8"><Error error={error}></Error></td></tr>}
+					<tr>
+						<td>{user.name}</td>
+						<td>{user.email}</td>
+						{possiblePermissions.map((permission) => (
+							<td key={permission}>
+								<label htmlFor={`${user.id}-permission-${permission}`} />
+								<input
+									id={`${user.id}-permission-${permission}`}
+									type="checkbox"
+									checked={this.state.permissions.includes(permission)}
+									value={permission}
+									onChange={this.handlePermissionChange}
+								/>
+							</td>
+						))}
+						<td>
+							<SickButton type="button" disabled={loading} onClick={updatePermissions}>
+								Updat{loading ? 'ing' : 'e'}
+							</SickButton>
+						</td>
+					</tr>
+                    </>
+				)}
+			</Mutation>
+		);
+	}
 }
 
 export default Permissions;
