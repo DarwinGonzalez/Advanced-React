1bf0a86632b5850df3d4ebdbd0fa05b2e0f022e4
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-12-10 13:17:28 +0100

    Start create add to cart items back/front

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,6 +6,7 @@ import ItemStyles  from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import DeleteItem from './DeleteItem';
 import formatMoney from '../lib/formatMoney';
+import AddToCart from './AddToCart';
 
 class Item extends Component {
     static PropTypes = {
@@ -34,7 +35,7 @@ class Item extends Component {
                 }}>
                     <a>Edit</a>
                 </Link>
-                <button>Add To Cart</button>
+                <AddToCart id={item.id}/>
                 <DeleteItem id={item.id}>Delete this Item</DeleteItem>
             </div>
         </ItemStyles>
