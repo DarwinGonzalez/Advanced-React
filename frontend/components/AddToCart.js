e1a8fc92d35c4036ede76d38c7471661bfce4890
Darwin Gonzalez <alu0100812794@ull.edu.es>
2019-12-11 09:50:32 +0100

    Adding items to the interface of the cart

--- frontend/components/AddToCart.js
+++ frontend/components/AddToCart.js
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { CURRENT_USER_QUERY } from './User';
 
 const ADD_TO_CART_MUTATION = gql`
-    mutation addToCart($id: ID!) {
-        addToCart(id: $id) {
-            id,
-            quantity
-        }
-    }
+	mutation addToCart($id: ID!) {
+		addToCart(id: $id) {
+			id
+			quantity
+		}
+	}
 `;
 class AddToCart extends React.Component {
-    render() {
-        const { id } = this.props;
-        return <Mutation mutation={ADD_TO_CART_MUTATION} variables={{id}}>{(addToCart) => (
-            <button onClick={addToCart}>Add To Cart 🛒</button>
-        )}</Mutation>
-    }
+	render() {
+		const { id } = this.props;
+		return (
+			<Mutation
+				mutation={ADD_TO_CART_MUTATION}
+				variables={{ id }}
+				refetchQueries={[ { query: CURRENT_USER_QUERY } ]}
+			>
+				{(addToCart, { loading}) => <button onClick={addToCart} disabled={loading}>Add{loading && 'ing'} To Cart 🛒</button>}
+			</Mutation>
+		);
+	}
 }
 
 export default AddToCart;
